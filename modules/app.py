fe3de344da3d9798b2d9e33fb72ec8d4db3073fb
sambapython@gmail.com <sambapython@gmail.com>
2017-06-23 09:38:55 +0530

    2017-06-23

---
+++ modules/app.py
@@ -0,0 +1,24 @@
+'''
+print fun()
+'''
+'''
+import f1
+print f1.fun()
+'''
+'''
+import f2
+'''
+"""
+from f2 import fun
+"""
+"""
+from f2 import fuin,fun1
+"""
+"""
+from f2 import fun,a,b
+print "*"*60
+print fun()
+"""
+import f2
+print "*"*60
+print f2.fun()
---
+++ modules/f2.py
@@ -0,0 +1,10 @@
+print "thi is f2"
+print "__name__--->",__name__
+a=10
+b=20
+def fun():
+	return "this is fun in f2.py modified mofied again"
+print "some details"
+print fun()
+print "some other statements"
+print "end of the program"
